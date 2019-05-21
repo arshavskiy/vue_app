af4c77955dd5c3bbafa19f5cafaf67c8ecb17b41
Pavel <arshavsky.pasha@gmail.com>
2019-05-22 00:36:43 +0300

    loaded

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+  <Avatars :msg="msg" :data="avatarsData"/> 
+  </div>
+</template>
+
+<script>
+import Avatars from './components/Avatar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Avatars
+  },
+
+  data() {
+    return {
+      msg : 'Avatars!!!',
+      avatarsData: [
+        {
+          title:'Pavel',
+          img:'logo.png',
+          isActive : true
+        },
+        {
+          title:'Dina',
+          img:'logo.png',
+          isActive : false
+        },
+        {
+          title:'Lev',
+          img:'logo.png',
+          isActive : false
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Avatar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1 v-if="msg">{{msg}}</h1>
+        <h1>Avatars:</h1>
+
+        <div :key="avatar.length" v-for="avatar in data">
+            <div>
+                <h3>
+                    {{avatar.title}}
+                </h3>
+                <i :style="{ 'background': 'url(./assets/' + avatar.img + ')' }"></i>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Avatars',
+    props: [ 'msg', 'data']
+}
+</script>
+
+<style>
+
+</style>
